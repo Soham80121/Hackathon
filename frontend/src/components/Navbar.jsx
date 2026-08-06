8ac94bff70288ef974a00dff80b5408d875a08a4
Soham80121 <2k.soham.025@gmail.com>
2026-08-06 21:03:50 +0530

    Completed frontend structure and landing page

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+function Navbar() {
+
+    return (
+
+        <nav className="bg-white shadow-md p-4">
+
+            <h1 className="text-2xl font-bold">
+
+                HRFlow AI
+
+            </h1>
+
+        </nav>
+
+    );
+
+}
+
+export default Navbar;
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,30 @@
+import Navbar from "../components/Navbar";
+function Home() {
+
+  return (
+
+    <>
+
+      <Navbar />
+
+      <div className="min-h-screen bg-gray-100 flex justify-center items-center">
+
+        <div>
+
+          <h1 className="text-5xl font-bold">
+
+            Welcome to HRFlow AI
+
+          </h1>
+
+        </div>
+
+      </div>
+
+    </>
+
+  );
+
+}
+
+export default Home;
