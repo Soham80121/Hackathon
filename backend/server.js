1ce17a221bcab008b71ab491a34945f4aed49cd9
Soham80121 <2k.soham.025@gmail.com>
2026-08-07 13:41:30 +0530

    Authentication setup completed

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,28 @@
-const express = require("express");
-const cors = require("cors");
+require("dotenv").config();
 
-const app = express();
+const express=require("express");
+const cors=require("cors");
+
+const connectDB=require("./config/db");
+
+const app=express();
+
+connectDB();
 
 app.use(cors());
+
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Backend Running Successfully");
+app.get("/",(req,res)=>{
+
+res.send("HRFlow Backend Running");
+
 });
 
-const PORT = 5000;
+const PORT=process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(PORT,()=>{
+
+console.log(`Server running on ${PORT}`);
+
 });
